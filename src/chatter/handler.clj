598525f946d3317fe42441e58ce58e9885225a98
Millie Walsh <millie.walsh@code42.com>
2015-02-02 18:23:22 -0600

    vector to list and other cleanup

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -2,7 +2,12 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [hiccup.page :as page]))
+            [ring.middleware.params :refer [wrap-params]]
+            [hiccup.page :as page]
+            [hiccup.form :as form]
+            [ring.util.anti-forgery :as anti-forgery]))
+
+(def messages (atom '()))
 
 (defn generate-message-view
   "this generates the html for displaying messags"
@@ -11,11 +16,28 @@
    [:head
     [:title "chatter"]]
    [:body
-    [:h1 "Our Chat App"]]))
+    [:h1 "Our Chat App"]
+    [:p
+     (form/form-to
+      [:post "/"]
+      (anti-forgery/anti-forgery-field )
+      "Name: " (form/text-field "name")
+      "Message: " (form/text-field "message")
+      (form/submit-button "Submit"))]
+    [:p
+     [:table
+      (map (fn [m] [:tr [:td (:name m)] [:td (:message m)]]) @messages)]]]))
+
+(defn update-messages!
+  "this will update the message list"
+  [name message]
+  (swap! messages conj  {:name name :message message}))
 
 (defroutes app-routes
   (GET "/" [] (generate-message-view))
+  (POST "/" {params :params} (do
+                               (update-messages! (get params "name") (get params "message"))
+                               (generate-message-view)))
   (route/not-found "Not Found"))
 
-(def app
-  (wrap-defaults app-routes site-defaults))
+(def app (wrap-params app-routes))
